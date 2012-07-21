00ffa52a4aca57f65db50307dc1f8d5bb9febe10
Ivan Kanakarakis <ivan.kanak@gmail.com>
2012-07-21 17:33:09 +0300

    rework unmapnotify function

--- monsterwm.c
+++ monsterwm.c
@@ -1145,8 +1145,8 @@ void tile(Desktop *d, Monitor *m) {
  * so invisible windows do not exist on screen
  */
 void unmapnotify(XEvent *e) {
-    Desktop *d = NULL; Client *c = NULL;
-    if (wintoclient(e->xunmap.window, &c, &d)) removeclient(c, d);
+    Monitor *m = NULL; Desktop *d = NULL; Client *c = NULL;
+    if (wintoclient(e->xunmap.window, &c, &d, &m)) removeclient(c, d, m);
 }
 
 /**
